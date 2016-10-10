761c5541643541d460ad78df168ce6f5251f13fd
abhishek.c <abhishek.c@caritorsolutions.com>
2016-10-10 12:31:28 +0530

    intial chnages

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace myself\foobar;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
